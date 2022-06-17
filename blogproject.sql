6cc3fb558bd3fc8a5124026e85053e90f7169b67
Nancy Hendy <nancy.hendy@version1.com>
2022-06-17 02:19:24 +0100

    added delete,join and date queries

--- blogproject.sql
+++ blogproject.sql
@@ -30,10 +30,18 @@ insert into blog_post values (3,"Dublin",15/06/2022,"Dublin is scenic place", "x
 insert into blog_post values (4,"Donegal",15/06/2022,"Donegal is scenic place", "xyz", 2);
 insert into blog_post values (5,"Cork",15/06/2022,"Cork is scenic place", "xyz", 2);
 
-update blog_post set text_post="Donegal is best", picture="ZYX", date_time="01/01/2022", title="" where post_id=4;
+update blog_post set text_post="Donegal is best", picture="ZYX", date_time=01/01/2022, title="" where post_id=4;
 
 update author set first_name="Nancy", last_name="Hendy" where author_id=2;
 
+delete from blog_post where title = "Kerry";
+
+select concat(first_name,' ', last_name)as Name, title, text_post, picture, date_time from blog_post inner join author on blog_post.author = author.author_id;
+
+
+-- update blog_post set date_time =  STR_TO_DATE(date_time, '%d/%m/%Y'); //-- after we set date to varchar(10)
+
+ 
 select * from author;
 select * from blog_post;
 
