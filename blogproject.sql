cbf8dfbd03bc693041b806ff68a8c3797ae399a2
Neeraja Marri <mnrishi2002@gmail.com>
2022-06-24 16:05:58 +0000

    updating

--- blogproject.sql
+++ blogproject.sql
@@ -34,7 +34,7 @@ update blog_post set text_post="Donegal is best", picture="ZYX", date_time=01/01
 
 update author set first_name="Nancy", last_name="Hendy" where author_id=2;
 
-delete from blog_post where title = "Kerry";
+delete from blog_post where post_id = 16;
 
 select concat(first_name,' ', last_name)as Name, title, text_post, picture, date_time from blog_post inner join author on blog_post.author = author.author_id;
 
@@ -44,6 +44,24 @@ select concat(first_name,' ', last_name)as Name, title, text_post, picture, date
  
 select * from author;
 select * from blog_post;
+
+
+drop table blog_post;
+
+create table blog_post(
+post_id tinyint not null primary key auto_increment,
+title varchar(50) not null,
+date_time datetime not NULL default current_timestamp,
+text_post text,
+picture varchar(200),
+author tinyint not null,
+foreign key(author) references author (author_id));
+
+insert into blog_post values (1,"Ireland",15/06/2022,"Ireland is scenic place", "Ireland", 1);
+insert into blog_post values (2,"Egypt",16/06/2022,"Egypt is scenic place", "Egypt", 2);
+insert into blog_post values (3,"France",15/06/2022,"France is scenic place", "France", 1);
+
+
 
  
  
